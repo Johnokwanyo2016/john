d725a268afb6682a609031ce9c48e294987f541e
johnokwanyo2016 <johnokwanyo2016@gmail.com>
2024-01-22 13:05:13 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Give in your temperature:");
+
+
+        int x = 4;
+        int y =10;
+        System.out.println(x+y);
+        int [][]myNumbers={{1,2,3,4},{5,6,7,8,9}};
+        System.out.println(myNumbers[0][1]);
+        System.out.println("Hello John");
+        
+
+
+
+
+    }
+}
